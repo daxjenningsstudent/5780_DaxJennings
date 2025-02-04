6bfe75cb5f17ed981bcc030d0c9aeb2c1972239c
Dax Jennings <u1299229@umail.utah.edu>
2025-02-04 08:59:51 -0700

    Added lab2.c and set leds as solid or flashing

---
+++ Src/lab2.c
@@ -0,0 +1,32 @@
+#include <stm32f0xx_hal.h>
+#include "main.h"
+#include <assert.h>
+
+int lab2_main(void) {
+    HAL_Init(); // Reset of all peripherals, init the Flash and Systick
+    assert(HAL_GetTick() == 0); // Ensure HAL initialization resets tick counter
+    
+    SystemClock_Config(); //Configure the system clock
+
+    /* This example uses HAL library calls to control
+        the GPIOC peripheral. You’ll be redoing this code
+        with hardware register access. */
+
+    My_HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC
+
+    // Set up a configuration struct to pass to the initialization function
+    GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+                                GPIO_MODE_OUTPUT_PP,
+                                GPIO_SPEED_FREQ_LOW,
+                                GPIO_NOPULL};
+
+    My_HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC6, PC7, PC8 & PC9
+
+    My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET); // Start PC9 (Green) high
+
+    while (1) {
+        HAL_Delay(500); // Delay 500ms
+        // Toggle the output state of both PC8 and PC9
+        My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
+    }
+}
